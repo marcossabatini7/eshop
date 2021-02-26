6de76aa8aa655ff1112a944a4f487334b62e2f8e
marcossabatini7 <marcossabatini7@gmail.com>
2021-02-26 15:01:05 -0300

    User model

---
+++ src/app/models/User.model.ts
@@ -0,0 +1,10 @@
+export class UserModel {
+  constructor(
+    public email: string,
+    public image: string,
+    public name: string,
+    public roles: string[],
+    public token: string,
+    public username: string
+  ) {}
+}
